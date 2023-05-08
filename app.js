c93baa08206e69dfd4cf9305e3cf6eaae12290e4
davidskype1005 <davidskype1005@gmail.com>
2023-05-08 18:00:14 +0800

    css

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const favicon = require('serve-favicon');
+// const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+var cors = require('cors')
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+
+
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,14 +31,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/fontawesome', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(helmet());
+app.use(limiter);
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.render('404');
+  // next(createError(404));
 });
 
 // error handler
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: ["./views/**/*.ejs"],
   mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pri: {
+          main: "#DECDBA",
+          content: "#FBFAEE",
+          title: "#9F7E69",
+        },
+        nav: {
+          text:'#999'
+        },
+      },
+    },
   },
   plugins: [],
 };
