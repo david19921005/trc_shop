60408d0043c9a03edc55cdb9b4c9d3a2f304148a
davidskype1005 <davidskype1005@gmail.com>
2023-05-10 18:01:04 +0800

    css5/10-2

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,11 @@
 window.onload = function () {
+  let coverDom = ''
+
+  const elementBarIcon = document.querySelector(".bar");
+  const elementCartIcon = document.querySelector(".cart");
+  const elementCover = document.querySelector(".cover");
+
+
   const elementRwdLis = document.querySelectorAll(".rwd-li-toggle");
   elementRwdLis.forEach((e) => {
     e.addEventListener("click", (event) => {
@@ -6,14 +13,55 @@ window.onload = function () {
       const targetDom = document.querySelector(
         ".rwd-ul-toggle[data-id='" + id + "']"
       );
-      targetDom.classList.toggle("rwd-ul-toggle-leave");
       targetDom.classList.toggle("rwd-ul-toggle-enter");
+      targetDom.classList.toggle("rwd-ul-toggle-leave");
     });
   });
+
+
+  elementBarIcon.addEventListener("click", (event) => {
+    coverDom = 'bars';
+    elementCover.classList.toggle("hidden");
+    const elementBarList = document.querySelector(".rwd-list");
+    elementBarList.classList.toggle("rwd-list-leave");
+  });
+
+  elementCartIcon.addEventListener("click", (event) => {
+    coverDom = 'carts';
+    elementCover.classList.toggle("hidden");
+    const elementCartList = document.querySelector(".cart-list");
+    elementCartList.classList.toggle("cart-list-leave");
+    elementCartList.classList.toggle("cart-list-enter");
+  });
+
+  elementCover.addEventListener("click", (event) => {
+    console.log(coverDom === 'bars')
+    elementCover.classList.toggle("hidden");
+    if(coverDom === 'bars'){
+      const elementBarList = document.querySelector(".rwd-list");
+      elementBarList.classList.toggle("rwd-list-leave");
+      coverDom = ''
+    }else if(coverDom === 'carts'){
+      const elementCartList = document.querySelector(".cart-list");
+      elementCartList.classList.toggle("cart-list-leave");
+      elementCartList.classList.toggle("cart-list-enter");
+      coverDom = ''
+    }
+  });
+
+  
 };
-const coverToggle = () => {
-  const elementBarIcon = document.querySelector(".rwd-list");
-  const elementCover = document.querySelector(".cover");
-  elementCover.classList.toggle("hidden");
-  elementBarIcon.classList.toggle("rwd-list-leave");
-};
+
+
+
+// const toggleClass = document.querySelectorAll("[data-toggle-class]");
+  // if (toggleClass) {
+  //   for (let i = 0; i < toggleClass.length; i += 1) {
+  //     const elem = toggleClass[i];
+  //     elem.addEventListener("click", () => {
+  //       const className = elem.getAttribute("data-toggle-class");
+  //       const target = elem.getAttribute("data-target").slice(1);
+  //       document.getElementById(target).classList.toggle(className);
+  //     });
+  //   }
+  // }
